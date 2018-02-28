32263e18711d585af7b4b97467f526b1d1e79e62
sheilambadi <mbadiatieno@gmail.com>
2018-02-28 08:45:52 +0300

    initial commit

---
+++ app/src/main/java/com/wit/android/hospital/SpecialistsActivity.java
@@ -0,0 +1,13 @@
+package com.wit.android.hospital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpecialistsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specialists);
+    }
+}
